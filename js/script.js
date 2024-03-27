2602db27ef17423ebd0f01d209d5c023e8a005a1
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-27 19:25:39 +0100

    project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!',
+        image: 'https://www.narutogt.it/files/gallerie/utenti/usr-6682/nuovo%20sharingan%20ipnotico%20eterno%20di%20sasuke.png'
+    }
+    }
+  }).mount('#app')
